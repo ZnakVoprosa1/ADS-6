a41425f564db514e773005e2a632a4756475e874
Степан <184nikiforov7b@mail.ru>
2025-05-02 11:53:21 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,65 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(const T& d) : data(d), next(nullptr) {}
+    };
+    
+    Node* head;
+
+public:
+    TPQueue() : head(nullptr) {}
+
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    // Вставка элемента в очередь
+    void push(const T& item) {
+        Node* newNode = new Node(item);
+        if (!head || comparePriorities(item, head->data)) {
+            // вставляем в начало
+            newNode->next = head;
+            head = newNode;
+        } else {
+            // ищем позицию для вставки
+            Node* current = head;
+            while (current->next && comparePriorities(current->next->data, item)) {
+                current = current->next;
+            }
+            // вставляем после current
+            newNode->next = current->next;
+            current->next = newNode;
+        }
+    }
+
+    // Извлечение элемента из очереди
+    T pop() {
+        if (!head) {
+            throw std::out_of_range("Очередь пуста");
+        }
+        Node* temp = head;
+        T data = temp->data;
+        head = head->next;
+        delete temp;
+        return data;
+    }
+
+    bool empty() const {
+        return head == nullptr;
+    }
+
+private:
+    bool comparePriorities(const T& first, const T& second) {
+        return first.prior > second.prior;
+    }
 };
 
 struct SYM {
